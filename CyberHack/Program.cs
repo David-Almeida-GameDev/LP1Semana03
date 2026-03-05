1b981dc54288eb52ba3986effcdb6b9be1494200
David Almeida <davidalmeida.gamedev@gmail.com>
2026-03-05 16:33:34 +0000

    Adicionados os enums HackTool e SystemType

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CyberHack
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main(string[] args) 
         {
-            Console.WriteLine("Hello LP!");
+            HackTool myTool;
+
         }
     }
 }
